e7161b6e40dd079108b1f5525017ccc526dd036f
Upendra <upendra@unific.com>
2024-05-15 12:55:37 +0530

    Added configuration for the client connection manager in the camel-context.xml and modified the api calling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,30 +10,28 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         int count = 0;
-        while (count < 25) {
-            callAPI();
-            count++;
-        }
-
-    }
-
-    private static void callAPI() {
 //        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("camel-context.xml");
         CamelContext context = appContext.getBean(CamelContext.class);
+        while (count < 25) {
+            callAPI(context);
+            count++;
+        }
+        context.stop();
+        appContext.close();
+    }
+
+    private static void callAPI(CamelContext context) {
         context.start();
         context.getComponentNames();
         ProducerTemplate producerTemplate = context.createProducerTemplate();
         Exchange exchange = producerTemplate.request("http://localhost:8080/hello", new MyProcessor());
         System.out.println("Response from REST endpoint: " + exchange.getMessage().getBody(String.class));
-        context.stop();
-        appContext.close();
     }
 
     static class MyProcessor implements Processor {
